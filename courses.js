10fe670fb1db122ccd18a32f9e17f957f103af32
Riley Thompson <rthompson@medavail.com>
2019-11-06 17:35:27 -0500

    add prereq, antireq and color code by faculty

--- courses.js
+++ courses.js
@@ -3,33 +3,74 @@
 const api_token = process.env.api_token || 'd55d4614484986ea90da927aa3ad33b1';
 
 let uw_api = require('uwapi')(api_token);
+let rp = require('request-promise');
+let $ = require('cheerio');
 
 exports.search_by_subject = (data, res, view_path) => {
-	let subject = data.subject;
+  let subject = data.subject;
 	let lower_bound = Number(data.lower_bound);
 	let upper_bound = Number(data.upper_bound);
 
 	uw_api.courses({subject: subject}, {}).
 	then((response) => {
-		let courses = [];
-		response.forEach((course) => {
-			if (course.catalog_number >= lower_bound && course.catalog_number <= upper_bound) {
-				course.link = `http://www.ucalendar.uwaterloo.ca/1920/COURSE/course-${subject}.html#${subject}${course.catalog_number}`;
-				courses.push(course);
-			}
-		})
-
-		if (courses.length == 0) {
+		if (response.length < 1) {
 			res.render('partials/courses', {
 				subject: 'Sorry, no courses matched your search',
 				courses: []
 			})
 			return;
 		}
-	  	res.render(view_path, {
-			subject: subject,
-			courses: courses
-		})
+
+		let subject_url = `http://www.ucalendar.uwaterloo.ca/1920/COURSE/course-${subject}.html`;
+
+		rp(subject_url).then(function(html) {
+            let results = $('center > table > tbody', html);
+            let courses = [];
+            let j = 0; // result index
+
+            for (let i = 0; i < response.length; ++i) {
+                let course = response[i];
+
+                let scraped_course;
+                if (j < results.length) {
+                    scraped_course = results[j].firstChild.firstChild.firstChild.firstChild.next.data.split(' ').slice(0, 2).join(' ');
+                }
+
+                let api_course = course.subject + ' ' + course.catalog_number;
+
+                if (parseInt(course.catalog_number) >= lower_bound && parseInt(course.catalog_number) <= upper_bound) {
+                    course.prereq = '';
+                    course.antireq = '';
+                    if (scraped_course != undefined && scraped_course.includes(api_course)) {
+                        // console.log("scraped: ", scraped_course, " api: ", api_course);       
+                        for (let k = 0; k < results[j].children.length; ++k) {
+                            if (results[j].children[k].firstChild.firstChild.firstChild != undefined && results[j].children[k].firstChild.firstChild.firstChild.data != undefined) {
+                                if (results[j].children[k].firstChild.firstChild.firstChild.data.includes('Prereq')) {
+                                    course.prereq = results[j].children[k].firstChild.firstChild.firstChild.data.split(',').join(', ');
+                                } else if (results[j].children[k].firstChild.firstChild.firstChild.data.includes('Antireq')) {
+                                    course.antireq = results[j].children[k].firstChild.firstChild.firstChild.data.split(',').join(', ');
+                                }
+                            }
+                        }
+                        j += 1;     
+
+                        course.link = `http://www.ucalendar.uwaterloo.ca/1920/COURSE/course-${subject}.html#${subject}${course.catalog_number}`;
+                        courses.push(course);
+                    } else {
+                        // console.log("api: ", api_course);              
+                        course.link = `http://www.ucalendar.uwaterloo.ca/1920/COURSE/course-${subject}.html#${subject}${course.catalog_number}`;
+                        courses.push(course);
+                    }
+                }
+            }
+    
+              res.render(view_path, {
+                subject: subject,
+                courses: courses
+            })
+		}).catch(function(err) {
+			console.log(err);
+        })
 	})
 }
 
--- views/js/requests.js
+++ views/js/requests.js
@@ -94,6 +94,102 @@ const subjects = [
     'VCULT'
 ];
 
+let subject_color = {
+    'AFM': '#E78100',
+    'ACTSC': '#C60078',
+    'ANTH': '#E78100',
+    'AHS': '#0098A5',
+    'APPLS': '#E78100',
+    'AMATH': '#C60078',
+    'ARCH': '#8100B4',
+    'ARTS': '#E78100',
+    'ARBUS': '#E78100',
+    'AVIA': '#607000',
+    'BIOL': '#0073CE',
+    'BME': '#8100B4',
+    'BET': '#8100B4',
+    'CDNST': '#E78100',
+    'CHE': '#8100B4',
+    'CHEM': '#0073CE',
+    'CMW': '#E78100',
+    'CIVE': '#8100B4',
+    'CLAS': '#E78100',
+    'COGSCI': '#E78100',
+    'CO': '#C60078',
+    'COMMM': '#C60078',
+    'CS': '#C60078',
+    'COOP': '#FFD54F',
+    'CROAT': '#E78100',
+    'DAC': '#E78100',
+    'DUTCH': '#E78100',
+    'EARTH': '#0073CE',
+    'ECON': '#E78100',
+    'ECE': '#8100B4',
+    'ENGL': '#E78100',
+    'ENBUS': '#607000',
+    'ERS': '#607000',
+    'ENVE': '#8100B4',
+    'ENVS': '#607000',
+    'FINE': '#E78100',
+    'FR': '#E78100',
+    'GENE': '#8100B4',
+    'GEOG': '#607000',
+    'GEO': '#FFD54F',
+    'GER': '#E78100',
+    'GERON': '#0098A5',
+    'GBDA': '#E78100',
+    'GRK': '#E78100',
+    'HLTH': '#0098A5',
+    'HIST': '#E78100',
+    'HRM': '#E78100',
+    'HUMSC': '#E78100',
+    'INDEV': '#607000',
+    'INTST': '#E78100',
+    'ITAL': '#E78100',
+    'ITALST': '#E78100',
+    'JS': '#E78100',
+    'KIN': '#0098A5',
+    'INTEG': '#607000',
+    'LAT': '#E78100',
+    'LS': '#E78100',
+    'MSCI': '#8100B4',
+    'MNS': '#0073CE',
+    'MATBUS': '#C60078',
+    'MATH': '#C60078',
+    'MTHEL': '#C60078',
+    'ME': '#8100B4',
+    'MTE': '#8100B4',
+    'MEDVL': '#E78100',
+    'MUSIC': '#E78100',
+    'NE': '#8100B4',
+    'OPTOM': '#0073CE',
+    'PACS': '#E78100',
+    'PHARM': '#0073CE',
+    'PHIL': '#E78100',
+    'PHYS': '#0073CE',
+    'PLAN': '#607000',
+    'PSCI': '#E78100',
+    'PORT': '#E78100',
+    'PSYCH': '#E78100',
+    'PMATH': '#C60078',
+    'REC': '#0098A5',
+    'RS': '#E78100',
+    'RUSS': '#E78100',
+    'REES': '#E78100',
+    'SCI': '#0073CE',
+    'SCBUS': '#0073CE',
+    'SMF': '#E78100',
+    'STVV': '#FFD54F',
+    'SOC': '#E78100',
+    'SE': '#8100B4',
+    'SPAN': '#E78100',
+    'SPCOM': '#E78100',
+    'STAT': '#C60078',
+    'SYDE': '#8100B4',
+    'UNIV': '#FFD54F',
+    'VCULT': '#E78100'
+}
+
 function courses_by_subject(subject, lower_bound, upper_bound) {
     $.ajax({
         url: '/courses_by_subject',
@@ -109,6 +205,7 @@ function courses_by_subject(subject, lower_bound, upper_bound) {
             $('.remove-course').on('click', function(_ev) {
                 $(this.parentElement.parentElement).fadeOut();
             })
+            $('.course-block').css('border', '2px solid ' + subject_color[subject]);
         },
         error: function(jqXHR, textStatus, err) {
             console.error('Error:',  jqXHR.status, jqXHR.responseText, textStatus, err);
@@ -159,6 +256,7 @@ function course_by_search(search) {
             $('.remove-course').on('click', function(_ev) {
                 $(this.parentElement.parentElement).fadeOut();
             })
+            $('.course-block').css('border', '2px solid ' + subject_color[search_data.subject]);
         },
         error: function(jqXHR, textStatus, err) {
             console.error('Error:',  jqXHR.status, jqXHR.responseText, textStatus, err);
